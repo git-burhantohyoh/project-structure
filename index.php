2f9be16b923aabaf9dafb1d9c4c069dfcce1819d
BURHAN TOHYOH <burhantohyoh.work@gmail.com>
2020-10-25 18:47:14 +0700

    All change

--- index.php
+++ index.php
@@ -1,30 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
 
-    <script src="js/app.js"></script>
-  </head>
-  <body>
-    hi
-    <script>
-      const { value: password } = await Swal.fire({
-        title: "Enter your password",
-        input: "password",
-        inputLabel: "Password",
-        inputPlaceholder: "Enter your password",
-        inputAttributes: {
-          maxlength: 10,
-          autocapitalize: "off",
-          autocorrect: "off",
-        },
-      });
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TOEIC PSU Phuket Test Online System</title>
 
-      if (password) {
-        Swal.fire(`Entered password: ${password}`);
-      }
-    </script>
-  </body>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <!-- style.css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="jumbotron title">
+        <div class="">
+            <h1 class="display-4 head-title">TOEIC PSU</h1>
+            <p class="sub-head-title">Prince of Songkla University Phuket Campus</p>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12 btn-user">
+                    <a class="btn btn-success btn-lg" href="auth/login.php" role="button">Normal Test</a>
+                    <a class="btn btn-primary btn-lg" href="auth/psupassport.php" role="button">PSU Passport</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- Optional Javascript -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+
 </html>
